59aeda83d6f4f45105a86cf9698abf3e046b0c70
Andres Castillo <andres.castillo@meliusid.com>
2025-05-13 18:54:36 -0500

    feat: Implement pagination and JSON rendering for tasks and users in controllers

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::TasksController < ApplicationController
+  def index
+    tasks = current_user.tasks.page(params[:page]).per(20)
+    render json: tasks, meta: pagination_meta(tasks)
+  end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::UsersController < ApplicationController
   def index
+    users = User.page(params[:page]).per(20)
+    render json: users, meta: pagination_meta(users)
   end
 
   def show
+    user = User.find(params[:id])
+    render json: user
   end
 end
